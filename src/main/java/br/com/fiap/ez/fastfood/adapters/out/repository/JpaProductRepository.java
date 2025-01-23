840857e457c343121ee59b175361f3da6fb28453
tchfer <tchfernando@gmail.com>
2025-01-22 22:47:10 -0300

    Ajustar repositories

--- src/main/java/br/com/fiap/ez/fastfood/adapters/out/repository/JpaProductRepository.java
+++ src/main/java/br/com/fiap/ez/fastfood/adapters/out/repository/JpaProductRepository.java
@@ -9,14 +9,9 @@ import java.util.List;
 
 public interface JpaProductRepository extends JpaRepository<ProductEntity, Long> {
 
-	@Query(nativeQuery = true, value = "SELECT * FROM EZ_FASTFOOD.PRODUCT WHERE id = :id")
+	@Query(nativeQuery = true, value = "SELECT * FROM EZ_FASTFOOD_CATALOG.PRODUCT WHERE id = :id")
 	ProductEntity findProductById(@Param("id") Long id);
 
-	@Query(nativeQuery = true, value = "SELECT * FROM EZ_FASTFOOD.PRODUCT WHERE category_id = :id")
+	@Query(nativeQuery = true, value = "SELECT * FROM EZ_FASTFOOD_CATALOG.PRODUCT WHERE category_id = :id")
 	List<ProductEntity> findProductByCategoryId(@Param("id") Long id);
-
-	@Query(nativeQuery = true, value = "SELECT COUNT(*) > 0 " + "FROM EZ_FASTFOOD.ORDER_ITEMS oi "
-			+ "JOIN EZ_FASTFOOD.PRODUCT p ON oi.product_id = p.id " + "WHERE p.id = :id")
-	boolean isProductAssociatedWithOrderItems(@Param("id") Long productId);
-
 }
