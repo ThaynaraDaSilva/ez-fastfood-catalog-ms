d56b4d87b2f65ec1b7ca6f494ba44cc9992903d9
tchfer <tchfernando@gmail.com>
2025-01-22 21:22:30 -0300

    Projeto rodando

---
+++ src/main/java/br/com/fiap/ez/fastfood/adapters/out/repository/JpaCategoryRepository.java
@@ -0,0 +1,14 @@
+package br.com.fiap.ez.fastfood.adapters.out.repository;
+
+import br.com.fiap.ez.fastfood.infrastructure.persistence.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface JpaCategoryRepository  extends JpaRepository<CategoryEntity, Long>{
+
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM EZ_FASTFOOD.CATEGORY WHERE id = :id")
+	CategoryEntity findCategoryById(@Param("id") Long id);
+}
--- src/main/java/br/com/fiap/ez/fastfood/infrastructure/config/UseCaseConfiguration.java
+++ src/main/java/br/com/fiap/ez/fastfood/infrastructure/config/UseCaseConfiguration.java
@@ -1,26 +1,17 @@
-package br.com.fiap.ez.fastfood.infrastructure.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import br.com.fiap.ez.fastfood.adapters.out.http.CatalogHttpClient;
-import br.com.fiap.ez.fastfood.adapters.out.http.PaymentHttpClient;
-import br.com.fiap.ez.fastfood.adapters.out.http.UserHttpClient;
-import br.com.fiap.ez.fastfood.adapters.out.messaging.PaymentPublisher;
-import br.com.fiap.ez.fastfood.application.usecases.OrderUseCase;
-
-import br.com.fiap.ez.fastfood.domain.repository.OrderRepository;
-
-
-@Configuration
-public class UseCaseConfiguration {
-
-	
-	@Bean
-	public OrderUseCase orderUseCase(OrderRepository orderRepository, 
-			CatalogHttpClient catalogHttpClient,
-			UserHttpClient userHttpClient,
-			PaymentPublisher paymentPublisher) {
-		return new OrderUseCase(orderRepository,catalogHttpClient,userHttpClient,paymentPublisher);
-	}
-}
+//package br.com.fiap.ez.fastfood.infrastructure.config;
+//
+//import br.com.fiap.ez.fastfood.application.usecases.ProductUseCase;
+//import br.com.fiap.ez.fastfood.domain.repository.CategoryRepository;
+//import br.com.fiap.ez.fastfood.domain.repository.ProductRepository;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class UseCaseConfiguration {
+//
+//	@Bean
+//	public ProductUseCase productUseCase(ProductRepository productRepository,
+//										 CategoryRepository categoryRepository) {
+//		return new ProductUseCase(productRepository, categoryRepository);
+//	}
+//}
