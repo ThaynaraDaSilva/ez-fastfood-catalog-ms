ee83a841855703ee39b8bcdf27d96e08469e7628
tchfer <tchfernando@gmail.com>
2025-01-22 22:48:41 -0300

    Ajustar configuração de Beans

--- src/main/java/br/com/fiap/ez/fastfood/infrastructure/config/RepositoryConfiguration.java
+++ src/main/java/br/com/fiap/ez/fastfood/infrastructure/config/RepositoryConfiguration.java
@@ -1,18 +1,26 @@
-//package br.com.fiap.ez.fastfood.infrastructure.config;
-//
-//import br.com.fiap.ez.fastfood.adapters.out.repository.JpaOrderRepository;
-//import br.com.fiap.ez.fastfood.adapters.out.repository.OrderRepositoryImpl;
-//import br.com.fiap.ez.fastfood.domain.repository.OrderRepository;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//
-//@Configuration
-//public class RepositoryConfiguration {
-//
-//	@Bean
-//	public OrderRepository orderRepository(JpaOrderRepository orderJpaRepository) {
-//		return new OrderRepositoryImpl(orderJpaRepository);
-//	}
-//
-//}
+package br.com.fiap.ez.fastfood.infrastructure.config;
+
+import br.com.fiap.ez.fastfood.adapters.out.repository.CategoryRepositoryImpl;
+import br.com.fiap.ez.fastfood.adapters.out.repository.JpaCategoryRepository;
+import br.com.fiap.ez.fastfood.adapters.out.repository.JpaProductRepository;
+import br.com.fiap.ez.fastfood.adapters.out.repository.ProductRepositoryImpl;
+import br.com.fiap.ez.fastfood.domain.repository.CategoryRepository;
+import br.com.fiap.ez.fastfood.domain.repository.ProductRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RepositoryConfiguration {
+
+	@Bean
+	public ProductRepository productRepository(JpaProductRepository jpaProductRepository) {
+		return new ProductRepositoryImpl(jpaProductRepository);
+	}
+
+	@Bean
+	public CategoryRepository categoryRepository(JpaCategoryRepository jpaCategoryRepository) {
+		return new CategoryRepositoryImpl(jpaCategoryRepository);
+	}
+
+}
